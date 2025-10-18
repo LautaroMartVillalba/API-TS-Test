2a967dcf0ac9667909c788922e5e9443de25a5b3
Lautaro Villalba <lautaromartinvillalba@gmail.com>
2025-10-17 22:47:13 -0300

    feat(auth): enhance login and refresh token handling

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -17,21 +17,22 @@ export class AuthController{
     async login(
         @Res({passthrough: true}) response: Response,
         @Body() dto: {email: string, password:string}
-    ){
-        console.log('AuthController.login received dto:', dto);
+        ){
         const { accessToken, refreshToken } = await this.authService.login(dto.email, dto.password);
 
         response.cookie('jwt', accessToken, {
             httpOnly: true,
             secure: true,
             maxAge: 1000 * 60 * 15,
-        });
+            }
+        );
 
         response.cookie('refresh', refreshToken, {
             httpOnly: true,
             secure: true,
             maxAge: 1000 * 60 * 60 * 24 * 7,
-        });
+            }
+        );
 
         return {mesagge: 'Login succesfully.'};
     }
@@ -39,6 +40,7 @@ export class AuthController{
     @Post('/logout')
     logout(@Res({ passthrough: true }) response: Response) {
       response.cookie('jwt', '', { maxAge: 0 });
+      response.cookie('refresh', '', { maxAge: 0 });
       
       return { message: 'Logout successful' };
     }
@@ -50,24 +52,17 @@ export class AuthController{
             throw new UnauthorizedException("Refresh token does not exists,");
         }
 
-        const payload = await this.jwtService.verifyAsync(tokenRefresh);
-
-        const user = await this.userService.findRawByEmailForAuth(payload.sub)
-
-        if(!user){
-            throw new Error("user not found");
-        }
-
-        const newAccessToken =  await this.jwtService.signAsync({sub: user.email, privileges: user.privileges});
+        const jwt = await this.authService.refreshToken(req);
 
         response.cookie(
             'jwt',
-            newAccessToken,
+            jwt,
             {
                 secure: true,
                 httpOnly: true,
-                maxAge: 1000*60*15
-            });
+                maxAge: 1000 * 60 * 15
+            }
+        );
 
         return {message: "Refreshed succesfully"}
     }
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,5 +1,5 @@
 /* eslint-disable prettier/prettier */
-import { Injectable } from "@nestjs/common";
+import { Injectable, UnauthorizedException } from "@nestjs/common";
 import { JwtService } from "@nestjs/jwt";
 import { UserService } from "src/user/user.service";
 
@@ -7,20 +7,43 @@ import { UserService } from "src/user/user.service";
 export class AuthService{
     constructor(private readonly jwtService: JwtService, private readonly userService: UserService){}
 
-        async login (email: string, password: string){
-                const user = await this.userService.findRawByEmailForAuth(email);
-                if (!user || user.password !== password) {
-                    throw new Error("Password doesn't matchs.");
-                }
-
-                const payload = {sub: user.email, privileges: user.privileges};
-                const accessToken = await this.jwtService.signAsync(payload);
-                const refreshToken = await this.jwtService.signAsync({sub: user.email})
-
-                return {accessToken, refreshToken};
+    async login (email: string, password: string){
+        const user = await this.userService.findRawByEmailForAuth(email);
+        if (!user || user.password !== password) {
+            throw new Error("Password doesn't matchs.");
         }
+
+        const payload = {sub: user.email, privileges: user.privileges};
+        const accessToken = await this.jwtService.signAsync(payload);
+        const refreshToken = await this.jwtService.signAsync({sub: user.email})
+
+        return {accessToken, refreshToken};
+    }
 
     async verifyToken(token: string){
         return this.jwtService.verifyAsync(token);
+    }
+
+    async refreshToken(req: any){
+        const tokenRefresh = req.cookies?.refresh;
+        console.log(tokenRefresh);
+            if(!tokenRefresh){
+                throw new UnauthorizedException("Refresh token does not exists,");
+            }
+        
+            const payload = await this.jwtService.verifyAsync(tokenRefresh);
+        
+        console.log(payload);
+            const user = await this.userService.findRawByEmailForAuth(payload.sub)
+        
+        console.log(user);
+            if(!user){
+                throw new Error("user not found");
+            }
+        
+            const newAccessToken =  await this.jwtService.signAsync({sub: user.email, privileges: user.privileges});
+
+        console.log(newAccessToken);
+            return newAccessToken;
     }
 }
